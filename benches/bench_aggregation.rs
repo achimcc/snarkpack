2051449223a5664e1a7b71ed8d9f736d6db8ffd1
Achim Schneider <achim@parity.io>
2023-06-05 22:37:53 +0200

    fmt

--- benches/bench_aggregation.rs
+++ benches/bench_aggregation.rs
@@ -3,7 +3,7 @@ mod aggregation;
 use aggregation::groth16_aggregation;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("groth16 aggregation", |b| b.iter(|| groth16_aggregation() ));
+    c.bench_function("groth16 aggregation", |b| b.iter(|| groth16_aggregation()));
 }
 
 criterion_group!(benches, criterion_benchmark);
